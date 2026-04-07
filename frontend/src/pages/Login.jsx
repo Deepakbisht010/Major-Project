6ca7bf155e4a4e847981fca03f46f5a5c2e691ac
Deepak Bisht <deepakbisht4050@gmail.com>
2026-04-07 20:22:44 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'  
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom' 
 import { useTranslation } from 'react-i18next'
 import { useAuth } from '../context/AuthContext'
 import Navbar from '../components/Navbar'
